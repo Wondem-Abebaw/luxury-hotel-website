7b4e5b4087c8b21188ff663938080e375e450433
Wondem Abebaw <wondem.abebaw@eaglelionsystems.com>
2026-02-20 22:11:00 +0300

    revalidate with tags

--- components/rooms-grid.tsx
+++ components/rooms-grid.tsx
@@ -21,7 +21,7 @@ export async function RoomsGrid() {
 
         {/* Rooms Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {rooms.map((room) => (
+          {rooms.map((room:any) => (
             <div
               key={room.id}
               className="group rounded-2xl overflow-hidden shadow-lg"
