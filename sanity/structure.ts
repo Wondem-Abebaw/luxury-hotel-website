4ae1a0f7fdc9006704c88d0b0f00c010a5965e07
Wondem A <wondem.abebaw@eaglelionsystems.com>
2026-03-03 16:57:10 +0300

    minor change in structure

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,7 +1,22 @@
+import { Calendar } from 'lucide-react'
 import type {StructureResolver} from 'sanity/structure'
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
     .title('Content')
-    .items(S.documentTypeListItems())
+    .items([
+      // Custom list items
+      S.listItem()
+        .title('Bookings')
+        .icon(Calendar)
+        .child(
+          S.documentList()
+            .title('All Bookings')
+            .filter('_type == "booking"')
+            .defaultOrdering([{ field: 'submittedAt', direction: 'desc' }])
+        ),
+
+      // Default document types
+      ...S.documentTypeListItems(),
+    ])
